49ef723f367e9567b1cdd20b73ab1267315b211a
Thejesh M <mthejesh361@gmail.com>
2026-01-10 14:15:16 -0500

    refactor : separated jpa and mongo repositories and fix dependency versions

--- src/main/java/com/shopzone/config/MongoConfig.java
+++ src/main/java/com/shopzone/config/MongoConfig.java
@@ -2,11 +2,10 @@ package com.shopzone.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoAuditing
-@EnableMongoRepositories(basePackages = "com.shopzone.repository")
+// Removed @EnableMongoRepositories from here as it is now in the Main class
 public class MongoConfig {
   // Enables @CreatedDate and @LastModifiedDate for MongoDB documents
 }
