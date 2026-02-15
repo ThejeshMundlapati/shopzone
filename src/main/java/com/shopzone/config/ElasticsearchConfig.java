99f1d797dd51d4cfcccadeb13d6768a33245dcd3
Thejesh M <mthejesh361@gmail.com>
2026-02-15 14:38:42 -0500

    add elasticSearch configuration

--- src/main/java/com/shopzone/config/ElasticsearchConfig.java
+++ src/main/java/com/shopzone/config/ElasticsearchConfig.java
@@ -1,2 +1,24 @@
-package com.shopzone.config;public class ElasticsearchConfig {
+package com.shopzone.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.shopzone.repository.elasticsearch")
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+  @Value("${spring.elasticsearch.uris:http://localhost:9200}")
+  private String elasticsearchUri;
+
+  @Override
+  public ClientConfiguration clientConfiguration() {
+    return ClientConfiguration.builder()
+        .connectedTo(elasticsearchUri.replace("http://", "").replace("https://", ""))
+        .withConnectTimeout(5000)
+        .withSocketTimeout(30000)
+        .build();
+  }
 }
